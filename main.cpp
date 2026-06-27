83f9d7b6c6996e510341de942f59d47f60ad6391
siam5ell <jishnusingh1304@gmail.com>
2026-06-27 11:32:11 +0530

    Ncurses library addition

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
-#include <iostream>
+#include <chrono>
+#include <ncurses.h>
+#include <thread>
 
-int main() {
-  std::cout << "Hello this is a basic pomodoro application." << std::endl;
+int main(int argc, char **argv) {
+  initscr();
+  refresh();
+  // Define base time point
+  auto next_tick = std::chrono::steady_clock::now();
+  for (int i = 10; i >= 1; i--) {
+    next_tick += std::chrono::seconds(1);
+    clear();
+    printw("%d seconds left\n", i);
+    refresh();
+    std::this_thread::sleep_until(next_tick);
+  }
+  next_tick += std::chrono::seconds(2);
+  clear();
+  printw("Surprise!!!! 10 seconds just passed. \n");
+  refresh();
+  std::this_thread::sleep_until(next_tick);
+  endwin();
 }
